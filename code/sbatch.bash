38a1c19fab702da3faf4999c8f2a9ba230e5c97b
justlaforge <justlaforge@gmail.com>
2025-04-27 14:45:16 -0400

    fixed bash script

--- code/sbatch.bash
+++ code/sbatch.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name="thread_matrices"
-#SBATCH --output="thread_matrices.%j.%N.out"
+#SBATCH --job-name="stencil_serial"
+#SBATCH --output="stencil_serial.%j.%N.out"
 #SBATCH --partition=compute
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16GB
+#SBATCH --mem=128GB
 #SBATCH --account=ccu108
 #SBATCH --export=ALL
-#SBATCH -t 00:30:00
+#SBATCH -t 00:90:00
 
 # Define the matrix sizes and thread counts
-N_values=(8) 
-C_values=(40000)  # rows and columns
+N_values=8
+C_values=(5000 10000 20000 40000)  # rows and columns
 P_values=(1) # (1 2 4 8 16 32 64 128)
 
 # Output CSV files
@@ -25,7 +25,7 @@ echo "Iterations,Rows,Cols,OverallTime,WorkTime" > $SERIAL_FILE
 for C in "${C_values[@]}"; do
     ./make-2d A $C
 
-    echo "Running serial version for C=$C with N=8 iterations."
+    echo "Running serial version for C=$C with N=${N_values} iterations."
     ./stencil-2d -n 8 -i A -o Out
 
 done
