9d46fa8884e1791ea9b79fb0ebc09c438e226eb8
justlaforge <justlaforge@gmail.com>
2025-04-27 13:33:18 -0400

    updated bash script with serial 40000 matrix at 8 iterations

--- code/sbatch.bash
+++ code/sbatch.bash
@@ -5,36 +5,29 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=128GB
+#SBATCH --mem=16GB
 #SBATCH --account=ccu108
 #SBATCH --export=ALL
-#SBATCH -t 00:180:00
+#SBATCH -t 00:30:00
 
 # Define the matrix sizes and thread counts
-N_values=(256 512 1024 2048 4096) 
-P_values=(1 2 4 8 16 32 64 128) # (1 2 4 8 16 32 64 128)
+N_values=(8) 
+C_values=(40000)  # rows and columns
+P_values=(1) # (1 2 4 8 16 32 64 128)
 
 # Output CSV files
-PARALLEL_FILE="parallelTime.csv"
+SERIAL_FILE="serialTime.csv"
 
 # Ensure the CSV files have headers
-echo "N,P,OverallTime,WorkTime" > $PARALLEL_FILE
+echo "Iterations,Rows,Cols,OverallTime,WorkTime" > $SERIAL_FILE
 
 # Loop over matrix sizes
-for N in "${N_values[@]}"; do
-    ./make_matrix A $N $N
-    ./make_matrix X $N $N
+for C in "${C_values[@]}"; do
+    ./make-2d A $C
 
-
-    # Loop over thread counts for parallel execution
-    for P in "${P_values[@]}"; do
-        echo "Running parallel version for N=$N with P=$P threads"
-        parallel_output=$(./omp_matrix_matrix A X Y2 $P)
-        overall_time_p=$(echo "$parallel_output" | grep "Overall Time:" | awk '{print $3}')
-        work_time_p=$(echo "$parallel_output" | grep "Work Time:" | awk '{print $3}')
-
-    done
+    echo "Running serial version for C=$C with N=8 iterations."
+    ./stencil-2d -n 8 -i A -o Out
 
 done
 
-echo "Experiment complete. Results stored in $PARALLEL_FILE."
+echo "Experiment complete. Results stored in $SERIAL_FILE."
