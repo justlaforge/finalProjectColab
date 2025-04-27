2d9ed36c549d026aff6e94056905e10ff3461614
kwwallace <kwwallace@coastal.edu>
2025-04-27 01:57:46 -0400

    modified:   code/Makefile 	modified:   code/make-2d.c 	modified:   code/print-2d.c 	modified:   code/stencil-2d-hybrid.c 	modified:   code/stencil-2d-mpi.c 	modified:   code/stencil-2d-omp.c 	modified:   code/stencil-2d-pth.c 	modified:   code/stencil-2d.c 	modified:   code/utilities.c 	modified:   code/utilities.h

--- code/stencil-2d.c
+++ code/stencil-2d.c
@@ -20,125 +20,129 @@
  * Errors:   Usage errors and file permission errors
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "utilities.c"
-#include <getopt.h>
-
-void usage(char **argv){
-    printf("Usage: %s -n <num iters> -i <in file> -o <out file> -d <debug: 0,1,2>\n", argv[0]);
-}
-
-// Set arguments
-void setArgs(int argc, char **argv, int *n, char **in, char **out, int *debug){
-	int opt;
-
-	while((opt = getopt(argc, argv, "n:i:o:v")) != -1){
-		switch(opt){
-			case 'n':
-				*n = atoi(optarg);
-				break;
-			case 'i':
-				*in = optarg;
-				break;
-			case 'o':
-				*out = optarg;
-				break;
-			case 'v':
-				*debug = atoi(optarg);
-				break;
-			default:
-				usage(argv);
-				exit(1);
-		}
-	}
-	if(*in == NULL ||*out == NULL){
-		perror("Files -i and -o must be provided");
-		exit(EXIT_FAILURE);
-	}
-    
-	
-}
-
-int main(int argc, char **argv){
-    int n=1,debug=0;
-    char *in = NULL;
-    char *out = NULL;
-    
-    //set args
-    setArgs(argc, argv, &n, &in, &out, &debug);
-
-    double *matrix;
-	double *newMatrix;
-    int rows, cols;
-
-    newMatrix = malloc(n*n*sizeof(double));
-    if (newMatrix == NULL) {
-       fprintf(stderr, "Can't allocate storage\n");
-       exit(-1);
-    }
-
-    Read_matrix(in, &matrix, &rows, &cols);
-    // Do something
-
-	newMatrix = matrix;
-	double *temp;
-	// Loop iterations
-	for(int o=0; o<n; o++){
-		// Loop rows
-		for(int i=1;i<rows-1;i++){
-			//Loop Cols
-			for(int j=1; j<cols-1;j++){
-				newMatrix[i * cols + j] = (
-					matrix[(i-1) * cols + (j-1)] + matrix[(i-1) * cols + j] + matrix[(i-1) * cols + (j+1)] +
-					matrix[i * cols + (j-1)]     + matrix[i * cols + j]     + matrix[i * cols + (j+1)] +
-					matrix[(i+1) * cols + (j-1)] + matrix[(i+1) * cols + j] + matrix[(i+1) * cols + (j+1)]
-				) / 9.0;
-				 
-			}
-		}
-		temp = matrix;
-		matrix = newMatrix;
-		newMatrix = temp;
-	}
-    Print_matrix(matrix, rows, cols);
-
-	write_memory_to_file(matrix,rows,cols,out);
-    
-/*
-     // Writing matrix to binary file
-     FILE *file = fopen(out, "wb");
-     if (!file) {
-         fprintf(stderr, "Error: Unable to open file for writing.\n");
-		 free(matrix);
-		 free(newMatrix);
-         return EXIT_FAILURE;
-     }
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include "utilities.c"
+ #include <getopt.h>
+ #include <time.h>
  
-     // Write the dimensions
-     if (fwrite(&cols, sizeof(int), 1, file) != 1 || fwrite(&cols, sizeof(int), 1, file) != 1) {
-         fprintf(stderr, "Error: Failed to write matrix dimensions.\n");
-         fclose(file);
-		 free(matrix);
-		 free(newMatrix);
-         return EXIT_FAILURE;
-     }
+ void usage(char **argv){
+	 printf("Usage: %s -n <num iters> -i <in file> -o <out file> -d <debug: 0,1,2>\n", argv[0]);
+ }
  
-     // Write the matrix data
-     if (fwrite(matrix, sizeof(double), cols * cols, file) != (size_t)(cols * cols)) {
-         fprintf(stderr, "Error: Failed to write matrix data.\n");
-         fclose(file);
-		 free(matrix);
-		 free(newMatrix);
-         return EXIT_FAILURE;
-     }
+ // Set arguments
+ void setArgs(int argc, char **argv, int *n, char **in, char **out, int *debug){
+	 int opt;
  
-     fclose(file);
-	*/
-
-
-    free(matrix);
-    free(newMatrix);
-    return 0;
-}
+	 while((opt = getopt(argc, argv, "n:i:o:v")) != -1){
+		 switch(opt){
+			 case 'n':
+				 *n = atoi(optarg);
+				 break;
+			 case 'i':
+				 *in = optarg;
+				 break;
+			 case 'o':
+				 *out = optarg;
+				 break;
+			 case 'v':
+				 *debug = atoi(optarg);
+				 break;
+			 default:
+				 usage(argv);
+				 exit(1);
+		 }
+	 }
+	 if(*in == NULL ||*out == NULL){
+		 perror("Files -i and -o must be provided");
+		 exit(EXIT_FAILURE);
+	 }
+	 
+	 
+ }
+ 
+ int main(int argc, char **argv){
+	 // ---- Timer Variables ----
+	 double startOvrll=0;
+	 double finishOvrll=0;
+	 double startWork=0;
+	 double finishWork=0;
+ 
+	 GET_TIME(startOvrll);
+	 
+	 int n=1,debug=0;
+	 char *in = NULL;
+	 char *out = NULL;
+	 
+	 //set args
+	 setArgs(argc, argv, &n, &in, &out, &debug);
+ 
+	 double *matrix;
+	 double *newMatrix;
+	 int rows, cols;
+ 
+	 newMatrix = malloc(n*n*sizeof(double));
+	 if (newMatrix == NULL) {
+		fprintf(stderr, "Can't allocate storage\n");
+		exit(-1);
+	 }
+ 
+	 Read_matrix(in, &matrix, &rows, &cols);
+	 
+	 
+	 GET_TIME(startWork);
+ 
+	 newMatrix = matrix;
+	 double *temp;
+	 // Loop iterations
+	 for(int o=0; o<n; o++){
+		 // Loop rows
+		 for(int i=1;i<rows-1;i++){
+			 //Loop Cols
+			 for(int j=1; j<cols-1;j++){
+				 newMatrix[i * cols + j] = (
+					 matrix[(i-1) * cols + (j-1)] + matrix[(i-1) * cols + j] + matrix[(i-1) * cols + (j+1)] +
+					 matrix[i * cols + (j-1)]     + matrix[i * cols + j]     + matrix[i * cols + (j+1)] +
+					 matrix[(i+1) * cols + (j-1)] + matrix[(i+1) * cols + j] + matrix[(i+1) * cols + (j+1)]
+				 ) / 9.0;
+				  
+			 }
+		 }
+		 temp = matrix;
+		 matrix = newMatrix;
+		 newMatrix = temp;
+	 }
+ 
+	 
+	 GET_TIME(finishWork);
+ 
+	 write_memory_to_file(matrix,rows,cols,out);
+	 
+ 
+	 free(matrix);
+	 free(newMatrix);
+ 
+	 GET_TIME(finishOvrll);
+ 
+	 
+	 double overAllTime = finishOvrll-startOvrll;
+	 double workTime = finishWork-startWork;
+  
+  
+	 // Open file to write timing data
+	 FILE *timeFile = fopen("serialTime.csv", "w");
+	 if (!timeFile) {
+		 fprintf(stderr, "Error: Unable to open file 'serialTime' for writing.\n");
+		 return EXIT_FAILURE;
+	 }
+  
+	 // Write the values to the file
+	 fprintf(timeFile, "%d,%d,%d,%f,%f\n", n, rows, cols, overAllTime, workTime);
+  
+	 // Close the file
+	 fclose(timeFile);
+ 
+	 return 0;
+ 
+ }
