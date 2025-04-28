0507343fe452b52a03c0def60768b8875031f8f4
justlaforge <justlaforge@gmail.com>
2025-04-28 17:22:14 -0400

    Did some stuff to pthreads

--- code/utilities.h
+++ code/utilities.h
@@ -1,6 +1,4 @@
 // Function protocols
-
-
 void Create_stencil(char prompt[], double A[], int m, int n);
 void Read_matrix(char* file_name, double **matrix, int *rows, int *cols);
 void Print_matrix(double* matrix, int rows, int cols);
@@ -22,16 +20,21 @@ void write_memory_to_file(double *A, int rows, int cols, char *fname);
 #endif
 
 
-/* Start of Justin's Section */
+/* For PThreads */
 
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define BLOCK_LOW(id,p,n) ((id)*(n)/(p))
+// given rank = id, give p = # processes (or threads), and given n, number of elements in 1 dimension, it will tell you the
+//starting index
+#define BLOCK_HIGH(id,p,n) (BLOCK_LOW((id)+1,p,n)-1)
+// given rank = id, give p = # processes (or threads), and given n, number of elements in 1 dimension, it will tell you the ending index
+#define BLOCK_SIZE(id,p,n) \
+(BLOCK_HIGH(id,p,n)-BLOCK_LOW(id,p,n)+1)
+// given rank = id, give p = # processes (or threads), and given n, number of elements in 1 dimension, it will tell you number of
+//elements that the given thread or process is responsible for
+#define BLOCK_OWNER(j,p,n) (((p)*((j)+1)-1)/(n))
+// given data index j (from [0, n-1], and given p = # processes (or threads), and given n, number of elements in 1 dimension, it will tell
+//you which RANK is responsible for that particular element, j.
+#define PTR_SIZE (sizeof(void*))
+#define CEILING(i,j) (((i)+(j)-1)/(j))
 
-
-/* End of Justin's Section */
-
-//--------------------------
-
-/* Start of Kyle's Section */
-
-
-
-/* End of Kyle's Section */
